91de8c98f66815cd5696876a52be616641c3113d
Julianto08 <jjulianto08@gmail.com>
2021-05-09 19:37:07 +0700

    Add footer and hamburger in mobile

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,21 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
 
+const hamburger = document.querySelector('#hamburger');
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+hamburger.addEventListener('click', function(event) {
+    drawer.classList.toggle('open');
+    event.stopPropagation();
+});
+
+hero.addEventListener('click', function() {
+    drawer.classList.remove('open');
+});
+
+main.addEventListener('click', function() {
+    drawer.classList.remove('open');
+});
 console.log('Hello Coders! :)');
