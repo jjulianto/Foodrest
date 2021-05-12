7a18060d3972910b075c8f46be1216964bbf41b9
Julianto08 <jjulianto08@gmail.com>
2021-05-12 14:10:24 +0700

    Fix some files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -24,13 +24,15 @@ main.addEventListener('click', function() {
 data.restaurants.forEach((restaurant) => {
     const dataset =`
         <article class="restaurant-item">
-            <img class="restaurant-thumbnail" src="${restaurant.pictureId}" alt="Restaurant Image">
+            <img class="restaurant-thumbnail" src="${restaurant.pictureId}" alt="${restaurant.name}">
             <div class="restaurant-content">
                 <p class="restaurant-city"><span class="material-icons">room</span> ${restaurant.city}</p>
                 <h1 class="restaurant-title">${restaurant.name}</h1>
                 <p class="restaurant-rating"><span class="material-icons">star</span> ${restaurant.rating}</p>
                 <p class="restaurant-description">${restaurant.description.substr(0, 100)}</p>
-                <a class="restaurant-button" href="#">Lihat Detail</a>
+                <div class="restaurant-parent">
+                    <a class="restaurant-button" href="#">Lihat Detail</a>
+                </div>
             </div>
         </article>
     `;
