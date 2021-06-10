7cc73da8d6d62d65f1f14fbab05b7fb526128e59
Julianto08 <jjulianto08@gmail.com>
2021-06-10 22:35:10 +0700

    Change API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   KEY: 'YOUR_API_KEY',
-  BASE_URL: 'https://dicoding-restaurant-api.el.r.appspot.com/',
-  BASE_IMG_URL: 'https://dicoding-restaurant-api.el.r.appspot.com/images',
+  BASE_URL: 'https://restaurant-api.dicoding.dev',
+  BASE_IMG_URL: 'https://restaurant-api.dicoding.dev/images',
 };
 
 export default CONFIG;
