94ca2eb18b7b03d0baeb4f51257c40dd59c31e29
[Azquiorra] <jjulianto08@gmail.com>
2021-07-28 02:21:29 +0700

    changed destination link on about us

--- src/scripts/views/components/navbar.js
+++ src/scripts/views/components/navbar.js
@@ -14,7 +14,7 @@ class Navbar extends HTMLElement {
               <ul class="nav-list">
                   <li class="nav-item"><a href="#">Home</a></li>
                   <li class="nav-item"><a href="#/favorite">Favorite</a></li>
-                  <li class="nav-item"><a href="https://github.com/Azquiorra" target="_blank" rel="noreferrer">About Us</a></li>
+                  <li class="nav-item"><a href="https://julianto.vercel.app/" target="_blank" rel="noreferrer">About Us</a></li>
               </ul>
           </nav>
       </div>
